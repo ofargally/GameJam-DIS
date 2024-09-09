565733051419e501190ed729e021c054e59e9806
Dylan O’Connor <dylanoconnor@Dylans-MacBook-Pro.local>
2024-09-09 04:18:21 -0700

    projectile movement

--- Assets/Scripts/ProjectileMovement.cs
+++ Assets/Scripts/ProjectileMovement.cs
@@ -1,6 +1,18 @@
 using UnityEngine;
+using Collections.UnityEngine
 
 public class ProjectileMovement
-{
+{  
+    // projectile movement will take a speed,
+    // angle, and mass and plan out a parabola
+    // for the projectile to move
+
+    private RigidBody2D rb;
+
+    void Start() {
+
+    }
+    void Update() {
     
+    }
 }
