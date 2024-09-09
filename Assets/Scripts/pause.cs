89bc2e5123775302281924ecdd3c53cdbb00863e
ZEKIMAR-2xO <omar@ozeki.ai>
2024-09-09 15:55:59 +0200

    changes

--- Assets/Scripts/pause.cs
+++ Assets/Scripts/pause.cs
@@ -6,14 +6,15 @@ public class pause : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)){
-            SceneManager.LoadSceneAsync(1);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadSceneAsync(2);
             Time.timeScale = 0;
         }
     }
