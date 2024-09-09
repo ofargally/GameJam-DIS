3648609193c7ef3c7df4fb1dd988e5de303d1866
Dylan O’Connor <dylanoconnor@Dylans-MacBook-Pro.local>
2024-09-09 03:59:12 -0700

    unknown edits

---
+++ Assets/Scripts/ProjectileClass.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ProjectileClass
+{
+    // Projectile class will instantiate with a set speed,
+    // angle, and mass. It will shoot into the air and 
+    // connect with a player or the ground, destroying itself
+
+    void Start() {
+
+    }
+    void Update() {
+
+    }
+}
