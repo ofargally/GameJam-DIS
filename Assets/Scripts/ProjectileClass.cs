f54260f03e785d43f3fee23505a07d52b600fe05
Dylan O’Connor <dylanoconnor@Dylans-MacBook-Pro.local>
2024-09-09 04:04:42 -0700

    ?

--- Assets/Scripts/ProjectileClass.cs
+++ Assets/Scripts/ProjectileClass.cs
@@ -6,7 +6,13 @@ public class ProjectileClass
     // angle, and mass. It will shoot into the air and 
     // connect with a player or the ground, destroying itself
 
+    public float speed;
+    public float angle;
+    public float mass;
+
+
     void Start() {
+
 
     }
     void Update() {
