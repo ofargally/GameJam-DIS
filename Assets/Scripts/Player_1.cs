9f8a6891ad3eabaaa2fc00aa480fccd6151c3e97
ZEKIMAR-2xO <omar@ozeki.ai>
2024-09-10 13:03:51 +0200

    added dino player

--- Assets/Scripts/Player_1.cs
+++ Assets/Scripts/Player_1.cs
@@ -5,7 +5,7 @@ public class Player_1 : PlayerController
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        Aim();
     }
 
     // Update is called once per frame
